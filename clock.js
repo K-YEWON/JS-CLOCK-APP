5edb6064dc25749b2f4cde1bb9550632edb075ad
yewon <yewon1914@gmail.com>
2021-02-04 16:40:55 +0900

    Saving the User Name part One

--- clock.js
+++ clock.js
@@ -6,11 +6,13 @@ function getTime(){
     const miunutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${miunutes}:${seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${miunutes < 10 ? `0${miunutes}` : miunutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    
 }
 
 function init() {
     getTime();
+    setInterval(getTime, 1000); //일정한 시간 간격으로 작업을 수행 setInterval(함수명, 실행시간);
 }
 
 init();
